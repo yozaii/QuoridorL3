c9386deda3b34aa77d06934e8c571a804b84b174
yozaii <73235941+yozaii@users.noreply.github.com>
2021-03-23 14:35:49 +0100

    Completion de deplacement, ajout de compteur de murs, amelioration menu deplacement

---
+++ src/pack/Pawn.java
@@ -0,0 +1,107 @@
+package pack;
+
+public class Pawn {
+	
+	private int x;
+	private int y;
+	private Board board;
+	private int yWin;
+	
+	//Constructeur
+	public Pawn(Board board, String Color) {
+		this.board = board;
+		if (Color == "White") {
+			board.GetTile(8, 0).SetHasPawn(true);
+			this.x = 8;
+			this.y = 0;
+			this.yWin = 16;
+		}
+		else if (Color == "Black") {
+			board.GetTile(8, 16).SetHasPawn(true);
+			this.x = 8;
+			this.y = 16;
+			this.yWin = 0;
+		}
+	}
+	
+	//Methode de déplacement, retourne 1 en cas de succes , -1 en cas d'echec
+	public int Move(String Direction) {
+		
+		int result;	//Le retour pour cette méthode
+		
+		switch(Direction) {
+		
+		case "Right":
+			
+			/*On peut se deplacer a droite si on est pas dans la case d'éxtremité droite && qu'il n'y a pas de mur a droite
+			 * && qu'il n'y a pas d'autre pion ou on veut se deplacer
+			 */
+			if (x<=14 && board.GetTile(x+1, y).GetHasWall() == false && board.GetTile(x+2, y).GetHasPawn()==false) {
+				board.GetTile(x, y).SetHasPawn(false);
+				board.GetTile(x+2, y).SetHasPawn(true);
+				this.x = x+2;
+				result = 1;
+			}
+			else result =-1;
+			break;
+
+		case "Left":
+			
+			/*On peut se deplacer a gauche si on est pas dans la case d'éxtremité gauche && qu'il n'y a pas de mur a gauche
+			 * && qu'il n'y a pas d'autre pion ou on veut se deplacer
+			 */
+			if (x>=2 && board.GetTile(x-1, y).GetHasWall() == false && board.GetTile(x-2, y).GetHasPawn()==false) {
+				board.GetTile(x, y).SetHasPawn(false);
+				board.GetTile(x-2, y).SetHasPawn(true);
+				this.x = x-2;
+				result = 1;
+			}
+			else result =-1;
+			break;
+			
+		case "Up":
+			
+			/*On peut se deplacer en haut si on est pas dans la case éxtremité d'en haut && qu'il n'y a pas de mur en haut
+			 * && qu'il n'y a pas d'autre pion ou on veut se deplacer
+			 */
+			if (y<=14 && board.GetTile(x, y+1).GetHasWall() == false && board.GetTile(x, y+2).GetHasPawn()==false) {
+				board.GetTile(x, y).SetHasPawn(false);
+				board.GetTile(x, y+2).SetHasPawn(true);
+				this.y = y+2;
+				result = 1;
+			}
+			else result =-1;
+			break;
+			
+		case "Down":
+			
+			/*On peut se deplacer en bas si on est pas dans la case éxtremité d'en bas && qu'il n'y a pas de mur en bas
+			 * && qu'il n'y a pas d'autre pion ou on veut se deplacer
+			 */
+			if (y>=2 && board.GetTile(x, y-1).GetHasWall() == false && board.GetTile(x, y-2).GetHasPawn()==false) {
+				board.GetTile(x, y).SetHasPawn(false);
+				board.GetTile(x, y-2).SetHasPawn(true);
+				this.y = y-2;
+				result = 1;
+			}
+			else result =-1;
+			break;
+			
+		default:
+			
+			result = -1;
+			break;
+		
+		}
+		
+		return result;
+	}
+	
+	public void PrintPosition() {
+		System.out.println("x: " + x + " y: " + y);
+	}
+	
+	
+	
+	
+}
