f849d6bed29220f48497685a552d0558af5842a5
yozaii <73235941+yozaii@users.noreply.github.com>
2021-04-21 17:19:21 +0100

    Petite modif dans eval

--- QuoridorL3/src/pack/AI.java
+++ QuoridorL3/src/pack/AI.java
@@ -2,26 +2,35 @@ package pack;
 
 public class AI {
 	
+	/*
+	 * La proximite au cases de fin donne une evaluation plus forte
+	 * + pour p1
+	 * - pour p2
+	 */
 	public static float eval (Board board, Pawn p1, Pawn p2) {
 		
 		
 		float eval = 0;
 		
 		
-		float eval1 = AStar.AlgoAStar(p1, board).size();
-		if (eval1 == 0) eval1 = 1000;
+		float eval1 = AStar.AlgoAStar(p1, board).size();//La longueur du AStar en cases
+		if (eval1 == 0) eval1 = 1000;//On peut pas diviser par 0, on met 1000
 		else {
-			eval1 = 1/eval1;
+			
+			//Fonction inverse, plus le chemin est long, plus l'evaluation s'approche du 0
+			eval1 = 1/eval1; 
 		}
 		
 		
-		float eval2 = AStar.AlgoAStar(p2, board).size();
-		if (eval2 == 0) eval2 = 1000;
+		float eval2 = AStar.AlgoAStar(p2, board).size();//La longueur du AStar en cases
+		if (eval2 == 0) eval2 = 1000;//On peut pas diviser par 0, on met 1000
 		else {
+			
+			//Fonction inverse, plus le chemin est long, plus l'evaluation s'approche du 0
 			eval2 = 1/eval2;
 		}
 		
-		eval = eval1 - eval2;
+		eval = (eval1 - eval2) * 10;
 		
 		return eval;
 	}
@@ -93,6 +102,76 @@ public class AI {
 				beta  = Math.min(beta, currentEval);
 				if (beta<= alpha)
 					break;
+			}
+			
+			String [] ret = {String.valueOf(minEval), bestMove};
+			//System.out.print("\tevalmin: " + ret[0] + "Move: " + ret[1]);
+			return ret;
+			
+		}
+	}
+	
+	/*Algorithme Minimax
+	 * retourne une tableau de chaine avec 2 valeurs: [eval, bestMove]
+	 */
+	public static String[] miniMaxNaive(Board board, boolean maxPlayer, int depth, Pawn p1, Pawn p2 ) {
+		
+		if (depth == 0||  p1.getY() == p1.getYWin()||  p2.getY() == p2.getYWin() ) {
+			
+			//Le retour ([eval, bestMove]
+			String [] ret = {String.valueOf(eval(board,p1,p2)), "None"};
+			//System.out.print("\t\t\teval: " + ret[0] + "Move: " + ret[1]);
+			return ret;
+		}
+		
+		String[] retHolder;
+		String move;
+		String bestMove = "None";
+		float maxEval;
+		float minEval;
+		float currentEval;
+		
+		if (maxPlayer) {	//Joueur max (p1)
+			
+			maxEval = Integer.MIN_VALUE;
+			
+			//Boucles pour tous les moves possibles
+			for (int i =0; i < p1.possibleMoves().size(); i++) {
+				
+				move = p1.possibleMoves().get(i);
+				p1.Move(move);
+				retHolder = miniMaxNaive(board, false, depth-1, p1, p2);
+				currentEval = Float.parseFloat(retHolder[0]);
+				p1.Move(p1.oppositeMove(move));//Annule le deplacement effectuer precedemment
+				
+				if (currentEval > maxEval) {
+					maxEval = currentEval;
+					bestMove = move;
+				}
+			}
+			
+			String [] ret = {String.valueOf(maxEval), bestMove};
+			//System.out.print("\tevalmax: " + ret[0] + "Move: " + ret[1]);
+			return ret;
+		}
+		
+		else { //Joueur min (p2)
+			
+			minEval = Integer.MAX_VALUE;
+			
+			//Boucles pour tous les moves possibles
+			for (int i = 0; i < p2.possibleMoves().size(); i++) {
+				
+				move = p2.possibleMoves().get(i);
+				p2.Move(move);
+				retHolder = miniMaxNaive(board, true, depth-1, p1, p2);
+				currentEval = Float.parseFloat(retHolder[0]);
+				p2.Move(p2.oppositeMove(move));//Annule le deplacement effectuer precedemment
+				
+				if (currentEval < minEval) {
+					minEval = currentEval;
+					bestMove = move;
+				}
 			}
 			
 			String [] ret = {String.valueOf(minEval), bestMove};
--- QuoridorL3/src/pack/Tests.java
+++ QuoridorL3/src/pack/Tests.java
@@ -7,15 +7,24 @@ public class Tests {
 	
 	public static void main(String args[]) {
 		
-		Scanner sc = new Scanner(System.in);
+		long startTime = System.nanoTime();
+		
+		
 		Board board = new Board();
 		Pawn p1 = new Pawn(board, "White");
 		Pawn p2 = new Pawn(board, "Black");
 		Interface.printGrid(board);
-		//LinkedList<Integer> l = AStar.AlgoAStar(p1, board);
-		String[] s = AI.miniMax(board, true, 11, Integer.MIN_VALUE, Integer.MAX_VALUE, p1, p2);
+		
+		int depth = 9;
+		String[] s = AI.miniMax(board, false, depth, Integer.MIN_VALUE, Integer.MAX_VALUE, p1, p2);
+		Interface.printGrid(board);
+		//String[] s = AI.miniMaxNaive(board, true, depth,  p1, p2);
 		System.out.println (s[0] + s[1]);
 		
+		
+		long endTime = System.nanoTime();
+		long duration = (endTime - startTime)/ 1000000;  //divide by 1000000 to get milliseconds.
+		System.out.println("Time = " +  duration + " ms");
 		
 	}
 
