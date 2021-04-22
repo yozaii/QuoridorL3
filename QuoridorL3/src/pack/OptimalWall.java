cd93a9aea0ab4b5d8b1c1b4289ecf33024436452
De Oliveira-Gill <nilam@deol.eu>
2021-04-22 20:35:03 +0200

    OptimalWall with 3 walls

--- QuoridorL3/src/pack/OptimalWall.java
+++ QuoridorL3/src/pack/OptimalWall.java
@@ -1,9 +1,23 @@
 package pack;
 
+import java.util.ArrayList;
+
 public class OptimalWall {
 
     private static int optimalX;
     private static int optimalY;
+
+/**
+ * Walls to try for a single tile :
+ * y+2 x-1
+ * y+2 x+1
+ * y+1 x-2
+ * y+1 x
+ * y x-1
+ * y x+1
+ * y-1 x-2
+ * y-1 x
+ */
 
     static public int optimalWall1(Pawn player, Board board){
 
@@ -109,47 +123,68 @@ public class OptimalWall {
         return bestWall;
     }
 
-    static public void optimalWall2(Pawn player, Board board) {
-        int x = player.getX();
-        int y = player.getY();
+    static public ArrayList<Integer> optimalWall2(Pawn player, Board board) {
 
-        int xWall = -1;
-        int YWall = -1;
+        //Initialisating the coordinates of the best walls 
+        int xWall1 = -1;int YWall1 = -1;
+        int xWall2 = -1;int YWall2 = -1;
+        int xWall3 = -1;int YWall3 = -1;
 
-        int bestWall = AStar.AlgoAStar(player, board).size();
+        //We are testing 3 walls to increase the chance to find the best wall through time
+        int bestWall1 = AStar.AlgoAStar(player, board).size();
+        int bestWall2 = AStar.AlgoAStar(player, board).size();
+        int bestWall3 = AStar.AlgoAStar(player, board).size();
 
-        if(bestWall < optimalWall1(player, board)) {
-            bestWall = optimalWall1(player, board);
-            xWall = optimalX;
-            YWall = optimalY;
+        //We test walls for the tile where the pawn is
+        if(bestWall1 < optimalWall1(player, board)) {
+            bestWall1 = optimalWall1(player, board);
+            xWall1 = optimalX;
+            YWall1 = optimalY;
         }
+        else if(bestWall2 < optimalWall1(player, board)) {
+            bestWall2 = optimalWall1(player, board);
+            xWall2 = optimalX;
+            YWall2 = optimalY;
+        }
+        else if(bestWall3 < optimalWall1(player, board)) {
+            bestWall3 = optimalWall1(player, board);
+            xWall3 = optimalX;
+            YWall3 = optimalY;
+        }
+
+        //We test the walls for all the tile in the best way to win
         for(int i : AStar.AlgoAStar(player, board)) {
-            x = i%17;
-            y = i/17;
+            int x = i%17;
+            int y = i/17;
 
             Pawn tempPlayer = new Pawn(board,x,y);
-            if(bestWall < optimalWall1(tempPlayer, board)) {
-                bestWall = optimalWall1(tempPlayer, board);
-                xWall = optimalX;
-                YWall = optimalY;
+            if(bestWall1 < optimalWall1(tempPlayer, board)) {
+                bestWall1 = optimalWall1(tempPlayer, board);
+                xWall1 = optimalX;
+                YWall1 = optimalY;
+            }
+            else if(bestWall2 < optimalWall1(tempPlayer, board)) {
+                bestWall2 = optimalWall1(tempPlayer, board);
+                xWall2 = optimalX;
+                YWall2 = optimalY;
+            }
+            else if(bestWall3 < optimalWall1(tempPlayer, board)) {
+                bestWall3 = optimalWall1(tempPlayer, board);
+                xWall3 = optimalX;
+                YWall3 = optimalY;
             }
         }
 
-        System.out.println("2 : x=" + xWall + " ; y=" + YWall);
+        //System.out.println("2 : x=" + xWall1 + " ; y=" + YWall1);
+        //System.out.println("2 : x=" + xWall2 + " ; y=" + YWall2);
+        //System.out.println("2 : x=" + xWall3 + " ; y=" + YWall3);
+        
+        ArrayList<Integer> bestWall = new ArrayList<Integer>();
+        bestWall.add(xWall1);bestWall.add(YWall1);
+        bestWall.add(xWall2);bestWall.add(YWall2);
+        bestWall.add(xWall3);bestWall.add(YWall3);
+        return bestWall;
 
     }
     
 }
-
-/**
- *  Mur a poser  :
- * y+2 x-1
- * y+2 x+1
- * y+1 x-2
- * y+1 x
- * y x-1
- * y x+1
- * y-1 x-2
- * y-1 x
- * 
- */
