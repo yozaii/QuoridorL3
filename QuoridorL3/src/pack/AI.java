a99efb8bc5710658888bca3022a086346dd4b46e
yozaii <73235941+yozaii@users.noreply.github.com>
2021-05-21 14:30:08 +0100

    Bug fix 2

--- QuoridorL3/src/pack/AI.java
+++ QuoridorL3/src/pack/AI.java
@@ -130,13 +130,13 @@ public class AI {
 		/*------------------------------------------------------------*/
 		if (maxPlayer) {
 			
-			boolean wallSuccess;
+			//boolean wallSuccess;
 			maxEval = Integer.MIN_VALUE;
 			ArrayList<String> pMoves = p1.possibleMoves();
-			ArrayList<Integer> optWalls =  OptimalWall.optimalWall2(p1, p2, board, 3);
-			ArrayList<Integer> optWallsEnemy = OptimalWall.optimalWall2(p2, p1, board, 2);
+			ArrayList<Integer> optWalls =  OptimalWall.optimalWall2(p1, p2, board, 5);
+			//ArrayList<Integer> optWallsEnemy = OptimalWall.optimalWall2(p2, p1, board, 2);
 			ArrayList<String> combination = UtilList.intStringComb(pMoves, optWalls);
-			combination = UtilList.intStringComb(combination, optWallsEnemy);
+			//combination = UtilList.intStringComb(combination, optWallsEnemy);
 			
 			/*------------------------------------------------------------*/
 			/*--------------Loop to branch all possible moves-------------*/
@@ -221,10 +221,10 @@ public class AI {
 			
 			minEval = Integer.MAX_VALUE;
 			ArrayList<String> pMoves = p2.possibleMoves();
-			ArrayList<Integer> optWalls = OptimalWall.optimalWall2(p2, p1, board, 3);
-			ArrayList<Integer> optWallsEnemy =  OptimalWall.optimalWall2(p1, p2, board, 2);
+			ArrayList<Integer> optWalls = OptimalWall.optimalWall2(p2, p1, board, 5);
+			//ArrayList<Integer> optWallsEnemy =  OptimalWall.optimalWall2(p1, p2, board, 2);
 			ArrayList<String> combination = UtilList.intStringComb(pMoves, optWalls);
-			combination = UtilList.intStringComb(combination, optWallsEnemy);
+			//combination = UtilList.intStringComb(combination, optWallsEnemy);
 			
 			/*------------------------------------------------------------*/
 			/*--------------Loop to branch all possible moves-------------*/
